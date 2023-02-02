26575e4d7bf874e8d7d523535938188f7fe44848
Dimitri Stallenberg <dimitrist@Dimitris-MacBook-Pro.local>
2023-02-02 15:15:30 +0100

    docs: add license headers

--- src/testcase/execution/JavaScriptRunner.ts
+++ src/testcase/execution/JavaScriptRunner.ts
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2020-2023 Delft University of Technology and SynTest contributors
+ *
+ * This file is part of SynTest Framework - SynTest Javascript.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {
   Datapoint,
   EncodingRunner,
@@ -45,9 +63,6 @@ export class JavaScriptRunner implements EncodingRunner<JavaScriptTestCase> {
     addLogs = false
   ): Promise<void> {
     const decodedTestCase = this.decoder.decode(testCase, targetName, addLogs);
-
-    // const transpiledTestCase = ts.transpileModule(decodedTestCase, { compilerOptions: { module: ts.ModuleKind.CommonJS }}).outputText
-    // await writeFileSync(filePath, transpiledTestCase);
 
     await writeFileSync(filePath, decodedTestCase);
   }
@@ -160,16 +175,6 @@ export class JavaScriptRunner implements EncodingRunner<JavaScriptTestCase> {
       for (const branchKey of Object.keys(instrumentationData[key].branchMap)) {
         const branch = instrumentationData[key].branchMap[branchKey];
         const hits = instrumentationData[key].b[branchKey];
-
-        // if (!hits.find((h) => h !== 0)) {
-        //   // if there are no hits the meta object is not created and thus we cannot query it
-        //   continue
-        // }
-        //
-        // if (!metaData[key] || !metaData[key].meta || !metaData[key].meta[branchKey]) {
-        //   continue
-        // }
-
         const meta = metaData?.[key]?.meta?.[branchKey];
 
         traces.push({
