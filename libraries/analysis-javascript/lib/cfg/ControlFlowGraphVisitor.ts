abbeb53a0c36b2d9e64071090dbe7b7ed82e6f00
Dimitri Stallenberg <tweet.dimitri@gmail.com>
2023-09-15 01:06:28 +0200

    fix: update packages

--- libraries/analysis-javascript/lib/cfg/ControlFlowGraphVisitor.ts
+++ libraries/analysis-javascript/lib/cfg/ControlFlowGraphVisitor.ts
@@ -28,11 +28,10 @@ import {
   Node,
   NodeType,
 } from "@syntest/cfg";
-import { getLogger } from "@syntest/logging";
+import { Logger, getLogger } from "@syntest/logging";
 
 export class ControlFlowGraphVisitor extends AbstractSyntaxTreeVisitor {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  protected static override LOGGER: any;
+  protected static override LOGGER: Logger;
 
   private _nodesList: Node[];
   private _nodes: Map<string, Node>;
